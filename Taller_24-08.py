a7814a51de49cc72ab21adeff7cd5a324e3b8af5
AlejandroVelascoG <javier.velasco8@gmail.com>
2018-08-27 17:31:13 -0500

    punto 3, punto 4, funcion de equivalencia

--- Taller_24-08.py
+++ Taller_24-08.py
@@ -63,33 +63,85 @@ print "La formula ",
 Inorder(formula)
 print " fue creada como un objeto!"
 
+# PUNTO 3
+
+def Vi(A, I):
+
+	if A.right == NULL:
+		return I(A.label)
+	if A.label == '-':
+		if(Vi(A.right)) == 1:
+			return 0
+		else:
+			return 1
+	if A.label == 'Y':
+		if(Vi(A.left) == 1) and (Vi(A.right) == 1):
+			return 1
+		else:
+			return 0
+	if A.label == 'O':
+		if(Vi(A.left) == 1) or (Vi(A.right) == 1):
+			return 1
+		else:
+			return 0
+	if A.label == '>':
+		if(Vi(A.left) == 0) or (Vi(A.right) == 1):
+			return 1
+		else:
+			return 0
+	if A.label == '<>':
+		if (Vi(A.left)) == (Vi(A.right)):
+			return 1
+		else:
+			return 0
+
 #EJERCICIO 4:
+
 letProp = ['p','q','r']
 interps = [] # Creamos la lsita de las posibles interpretaciones
 aux = {}     # Creamos la primera intrepretacion
 
-for i in letrasProposicionales:   
+for i in letProp:   
 	aux[i] = 1
 interps.append(aux) 
 # En este primer for inicializamos aux como verdadera para toda letra proposicional
-for i in letrasProposicionales:
+for a in letProp:
 	interps_aux = [j for j in interps]
 # Esta lista nos ayuda a tener las interpretaciones que llevemos
+
 	for i in interps_aux:
 		aux1 = {}
-		for j in range(len(letProp)):
-			for b in letProp:
-				if letProp[i] == letProp[b]:
-					aux1[b] == 1-i[j][b]
-# En este paso si estamos comparando letras proposicionales iguales les cambiamos la interpretacion
-				else:
-					aux1[b] = i[j][b]
-# Si no entonces se deja igual la interpretacion
+
+		for b in letProp:
+			if a== b:
+				aux1[b] = 1 - i[b]
+			else:
+				aux1[b] = i[b]
+		
 		interps.append(aux1)
+
+#		for j in range(len(letProp)):
+#			for b in letProp:
+#				if letProp[i] == letProp[b]:
+#					aux1[b] == 1-i[j][b]
+# En este paso si estamos comparando letras proposicionales iguales les cambiamos la interpretacion
+#				else:
+#					aux1[b] = i[j][b]
+# Si no entonces se deja igual la interpretacion
+#		interps.append(aux1)
 # Por ultimo se agrega a la lista de interpretaciones
 
-print "Interpretaciones :"
+print "Interpretaciones:"
 for i in interps:
 	print i
+
+# PUNTO 5
+
+def equiv(f1, f2):
+	for i in interps:
+		if Vi(f1, i) != Vi(f2, i):
+			print "las formulas no son equivalentes"
+		else:
+			print "las formulas son equivalentes"
 
 
